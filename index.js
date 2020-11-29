0a1b4c3fce4981c5134a7be1b020a4e799fcb69b
LAPTOP-BRJQG4NS\ykkim <kyk0416@gmail.com>
2020-11-29 12:28:02 +0700

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const db_config = require("./config/db-config.json");
-
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
 mongoose
-  .connect(db_config.url, {
+  .connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -16,5 +26,16 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world!!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  //  console.log(user);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
